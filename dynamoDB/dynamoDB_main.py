236f40742fbc6cdc57a00dd5f2047ff9944ebd8d
Ram Shiv <shiva.raman.se@gmail.com>
2023-07-08 14:31:23 +0000

    create dynamoDB Table

--- dynamoDB/dynamoDB_main.py
+++ dynamoDB/dynamoDB_main.py
@@ -1,5 +1,17 @@
 import boto3
 
-dynamodb = boto3.client('dynamodb')
 
-UserTable = dynamodb.create_table()
+dynamodb = boto3.client("dynamodb")
+
+
+# Create a DynamoDB Table
+response = dynamodb.create_table(
+    TableName="Ramshiv_test",
+    AttributeDefinitions=[{"AttributeName": "user", "AttributeType": "S"}],
+    KeySchema=[
+        {"AttributeName": "user", "KeyType": "HASH"},
+    ],
+    BillingMode="PAY_PER_REQUEST",
+)
+
+print(response)
