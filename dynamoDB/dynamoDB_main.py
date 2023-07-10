cd41600bf5736ff0d0c78de25dc88f814744ceb6
Ram Shiv <shiva.raman.se@gmail.com>
2023-07-10 19:41:16 +0000

    Added components, re-structured

--- dynamoDB/dynamoDB_main.py
+++ dynamoDB/dynamoDB_main.py
@@ -1,50 +1,40 @@
 import boto3
 
-
 dynamodb = boto3.client("dynamodb")
 
-
 # Create a DynamoDB Table
-
-response = dynamodb.create_table(
-    TableName="Ramshiv_test",
+create_response = dynamodb.create_table(
+    TableName="crc_test",
+    KeySchema=[{"AttributeName": "user", "KeyType": "HASH"}],
     AttributeDefinitions=[
         {"AttributeName": "user", "AttributeType": "S"},
-        {"AttributeName": "count", "AttributeType": "N"},
-    ],
-    KeySchema=[
-        {"AttributeName": "user", "KeyType": "HASH"},
-      #  {"AttributeName": "count", "KeyType": "RANGE"},
     ],
     BillingMode="PAY_PER_REQUEST",
 )
 
-
-# Insert data into DynamoDB
-
-insert_data = dynamodb.put_item(
-    TableName="Ramshiv_test", Item={"user": {"S": "default"}, "count": {"N": "0"}}
+# Add Initial Data to the Table
+data_response = dynamodb.put_item(
+    TableName="crc_test", Item={"user": {"S": "visitor"}, "count": {"N": "0"}}
 )
 
-insert_data = dynamodb.put_item(
-    TableName="Ramshiv_test", Item={"user": {"S": "test"}, "count": {"N": "1"}}
+
+# Retrieve Data Using Primary Key
+data_response = dynamodb.get_item(TableName="crc_test", Key={"user": {"S": "visitor"}})
+print(data_response["Item"])
+
+
+# Update Data in the Table
+update_response = dynamodb.put_item(
+    TableName="crc_test",
+    Item={"user": {"S": "visitor"}, "count": {"N": "1"}},
 )
 
-response_value = insert_data
 
-print(response_value)
-
-
-# Read from the DynamoDB
-data_response = dynamodb.get_item(
-    TableName="Ramshiv_test", Key={"user": {"S": "test"}, "count": {"N": "1"}}
+# Delete Data from the Table
+del_response = dynamodb.delete_item(
+    TableName="crc_test", Key={"user": {"S": "visitor"}}
 )
 
-print(data_response)
 
-
-# Delete the Table
-
-response = dynamodb.delete_table(TableName="Ramshiv_test")
-
-print(response)
+# Delete the DynamoDB Table
+delete_response = dynamodb.delete_table(TableName="crc_test")
--- dynamoDB/visitor_count.py
+++ dynamoDB/visitor_count.py
@@ -2,20 +2,20 @@ import boto3
 
 dynamodb = boto3.client("dynamodb")
 
-create_response = dynamodb.create_table(
-    TableName="crc_test",
-    KeySchema=[{"AttributeName": "user", "KeyType": "HASH"}],
-    AttributeDefinitions=[
-        {"AttributeName": "user", "AttributeType": "S"},
-    ],
-    BillingMode="PAY_PER_REQUEST",
-)
+# create_response = dynamodb.create_table(
+#     TableName="crc_test",
+#     KeySchema=[{"AttributeName": "user", "KeyType": "HASH"}],
+#     AttributeDefinitions=[
+#         {"AttributeName": "user", "AttributeType": "S"},
+#     ],
+#     BillingMode="PAY_PER_REQUEST",
+# )
 
 # Add Initial Data to the Table
 
-data_response = dynamodb.put_item(
-    TableName="crc_test", Item={"user": {"S": "visitor"}, "count": {"N": "0"}}
-)
+# data_response = dynamodb.put_item(
+#     TableName="crc_test", Item={"user": {"S": "visitor"}, "count": {"N": "0"}}
+# )
 
 # Read Data from the Table and Increment the Visitor Count
 
@@ -23,19 +23,30 @@ data_response = dynamodb.get_item(TableName="crc_test", Key={"user": {"S": "visi
 
 print(data_response.keys())
 
-print(data_response["Item"])
+# print(data_response["Item"])
 
-visited_count = int(data_response["Item"]["count"]["N"])
-
-print(visited_count)
+if "Item" in data_response:
+    visited_count = int(data_response["Item"]["count"]["N"])
+    print(f"Current Visited Count From Database : {visited_count}")
+else:
+    visited_count = 0
 
 visited_count += 1
 
-print(f"You have Visited the page with Count : {visited_count}")
+print(f"Visited Count after Incrementing : {visited_count}")
+
+print(f"This Page Has Been Visited {visited_count} Times")
+
+print(f"Visited Count To be Updated in DB : {visited_count}")
 
 update_response = dynamodb.put_item(
     TableName="crc_test",
     Item={"user": {"S": "visitor"}, "count": {"N": f"{visited_count}"}},
 )
 
-print(update_response)
+print(f"After Update : {update_response}")
+
+# Delete the DynamoDB Table
+
+# delete_response = dynamodb.delete_table(TableName="crc_test")
+# print(f"delete Response : {delete_response}")
